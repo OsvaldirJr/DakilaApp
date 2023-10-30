1893997c2191894655e5a1907861ad7a741aff79
Osvaldir Jr <software01@helpertecnologia.com.br>
2023-10-30 18:50:05 -0300

    ajuste do safe area

--- App.tsx
+++ App.tsx
@@ -2,12 +2,17 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import StackNavigator from './src/configs/routes';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { StatusBar } from 'react-native';
 
 
 export default function App() {
   return (
+    <>
     <NavigationContainer>
       <StackNavigator />
     </NavigationContainer>
+    <SafeAreaView style={{ flex: 1,  maxHeight:30, backgroundColor: 'black' }}></SafeAreaView>
+    </>
   );
 }
--- src/modules/home-module/index.tsx
+++ src/modules/home-module/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, SafeAreaView } from 'react-native';
 
 export default function HomeScreen() {
     return (
-        <View className='flex-1 items-center justify-center bg-white60'>
+       <>
+       <View className='flex-1 items-center justify-center bg-white60'>
             <Text className='text-black'>Home</Text>
         </View>
+         
+       </>
+        
     )
 }
