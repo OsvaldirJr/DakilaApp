436003ec8a20a29c03d9c10b64479572a3284d56
Osvaldir Jr <software01@helpertecnologia.com.br>
2023-12-17 11:01:00 -0300

    adicionando mapa

--- src/modules/map-module/index.tsx
+++ src/modules/map-module/index.tsx
@@ -2,35 +2,15 @@ import React from 'react';
 import { View } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import { styles } from './styles'
+import { getMapLink, ZaptMap, requestPermissions } from '@zapt-tech/react-native-zapt-sdk';
 
 export default function MapScreen() {
     return (
         <View style={styles.container}>
-            <MapView
-                style={{ width: '100%', height: '100%' }}
-                region={{
-                    latitude: -19.92465402898937,
-                    longitude: -55.17191908062471,
-                    latitudeDelta: 0.015,
-                    longitudeDelta: 0.0121,
-                }}
-                mapType='satellite'
-                zoomEnabled={false}
-                zoomControlEnabled={false}
-                zoomTapEnabled={false}
-                rotateEnabled={false}
-                scrollEnabled={false}
-                pitchEnabled={false}
-                loadingEnabled={true}
-                loadingBackgroundColor='#000000'
-                loadingIndicatorColor='#E2C535'
-            >
-                <Marker
-                    coordinate={{ latitude: -19.92465402898937, longitude: -55.17191908062471 }}
-                    title='Cidade Zigurats - Dakila Pesquisas'
-                    pinColor='#E2C535'
-                />
-            </MapView>
+            <ZaptMap
+        placeID={'-njt255g4yspfdlqu7uz'}
+        options={{floorId: 1, displayButtonList: false}}
+      />
         </View>
     )
 }
